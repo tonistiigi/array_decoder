b6fb4d5db973f1a84c2f07afe4cf2d8ab77efb6b
Tõnis Tiigi <tonistiigi@gmail.com>
2012-12-13 21:58:23 +0200

    Tests for array concat

--- test/index.js
+++ test/index.js
@@ -52,6 +52,31 @@ tests.buffers = function() {
   assert.deepEqual(result, [new Buffer('fooboobar'), new Buffer('baz')])
 }
 
+tests.simple_arrays = function() {
+  var decoder = new ArrayDecoder;
+  var result = decoder.write(['foo', ['bar'], ['baz']])
+  assert.deepEqual(result, ['foo', ['bar']])
+  result = decoder.write([['bar', '123'], ['abc']])
+  assert.deepEqual(result, [['bazbar', '123']])
+  result = decoder.write([['def']], true)
+  assert.deepEqual(result, [['abcdef']])
+}
+
+tests.complex_arrays = function() {
+  // many levels of concat + empty arrays
+  var decoder = new ArrayDecoder;
+  var result = decoder.write([['1', '2', '3']])
+  assert.strictEqual(result, null)
+  result = decoder.write([['4', '5']])
+  assert.strictEqual(result, null)
+  result = decoder.write([['6']])
+  assert.strictEqual(result, null)
+  result = decoder.write([['7'], [], ['8', '9']])
+  assert.deepEqual(result, [['1', '2', '34', '567'], []])
+  result = decoder.write([[], 'foo'], true)
+  assert.deepEqual(result, [['8', '9'], 'foo'])
+}
+
 Object.keys(tests).forEach(function(id) {
   console.log(id)
   tests[id]()
