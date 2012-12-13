2406aeab7bce40e16f7f6dc34716c4554cfc7fdc
Tõnis Tiigi <tonistiigi@gmail.com>
2012-12-13 22:13:30 +0200

    Array implementation(one level only)

--- array_decoder.js
+++ array_decoder.js
@@ -29,11 +29,38 @@ ArrayDecoder.prototype.mergeAuto = function(parts) {
     this.mergeBuffer(parts) : this.mergeString(parts)
 }
 
+ArrayDecoder.prototype.mergeArray = function(parts) {
+  var out = parts[0]
+  var merge = [out.pop()]
+  for (var i = 1; i < parts.length; i++) {
+    var part = parts[i]
+    if (part.length) {
+      merge.push(part.shift())
+    }
+    if (part.length) {
+      out.push(this.merge(merge))
+      merge = [part.pop()]
+      if (part.length) {
+        out = out.concat(part) // todo: optimize(concat later)
+      }
+    }
+  }
+  if (merge.length) {
+    out.push(this.merge(merge))
+  }
+  return out
+}
+
 ArrayDecoder.prototype.write = function(array, complete) { // complete:bool
   if (this._current.length && array.length) {
     this._current.push(array[0])
     if (array.length > 1 || complete) {
-      array[0] = this.merge(this._current)
+      if (this._current[0] instanceof Array) {
+        array[0] = this.mergeArray(this._current)
+      }
+      else {
+        array[0] = this.merge(this._current)
+      }
       this._current = []
     }
     else return null
